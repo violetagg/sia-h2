bf63bbc6ba8752184f82b8dcdbb2305077c6c535
Siavash Tahmureszadeh <stahmureszadeh@rapid7.com>
2022-01-12 13:19:28 -0500

    Added h2 configurations to demonstate the active connection and stream leak

--- siaserver/src/main/java/com/example/siaserver/SiaserverApplication.java
+++ siaserver/src/main/java/com/example/siaserver/SiaserverApplication.java
@@ -3,6 +3,7 @@ package com.example.siaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Mono;
+import reactor.netty.DisposableChannel;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.HttpProtocol;
 import reactor.netty.http.server.HttpServer;
@@ -17,11 +18,11 @@ public class SiaserverApplication {
 		DisposableServer server =
 				HttpServer.create()
 						.protocol(HttpProtocol.H2C)
-						.http2Settings(h2 -> h2.maxConcurrentStreams(1))
+						.http2Settings(h2 -> h2.maxConcurrentStreams(20))
 						.port(9192)
 						.handle((req, res) -> res.sendString(Mono
 								.just("sia")
-								.delayElement(Duration.ofSeconds(5))))
+								.delayElement(Duration.ofSeconds(2))))
 						.wiretap(true)
 						.bindNow();
 
