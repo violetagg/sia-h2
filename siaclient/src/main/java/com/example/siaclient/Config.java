525e4480116194d8616ed81a4e8a7ba5c5b02132
Violeta Georgieva <violetag@vmware.com>
2022-01-31 21:16:33 +0200

    GH issue 1982

--- siaclient/src/main/java/com/example/siaclient/Config.java
+++ siaclient/src/main/java/com/example/siaclient/Config.java
@@ -8,6 +8,8 @@ import reactor.netty.http.HttpProtocol;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
+import java.time.Duration;
+
 @Configuration
 public class Config {
 
@@ -15,7 +17,11 @@ public class Config {
     public HttpClient h2Client() {
 
         var provider = ConnectionProvider.builder("sia")
+                .fifo()
                 .maxConnections(100)
+                .maxLifeTime(Duration.ofSeconds(20))
+                .evictInBackground(Duration.ofSeconds(5))
+                .metrics(true)
                 .build();
 
         return HttpClient.create(provider)
